96e4dc4114ab3cd8a86c9af5297dfcb1b76e0be1
synfosec <thesyndikit@proton.me>
2024-06-04 08:47:55 -0700

    changes some stuff

--- scripts/userconfig.sh
+++ scripts/userconfig.sh
@@ -54,6 +54,7 @@ getConf() {
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 }
 
+# GET RUST
 get_rust() {
 	clear
 	echo -e "[*] Downloading Rust"
